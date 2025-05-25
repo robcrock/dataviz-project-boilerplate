5160800b2ec349edd3c290576469e33d27ebb774
Robert Crocker <rcrocker13@gmail.com>
2025-05-25 15:46:28 -0500

    Resolve TS errors.

--- src/app/(chart-types)/donut-chart/page.tsx
+++ src/app/(chart-types)/donut-chart/page.tsx
@@ -1,8 +1,8 @@
-'use client'
+"use client";
 
-import * as d3 from 'd3'
-import style from './styles.module.css'
-import { useRef } from 'react';
+import * as d3 from "d3";
+import style from "./styles.module.css";
+import { useRef } from "react";
 
 export type DataItem = {
   name: string;
@@ -16,13 +16,19 @@ export const data: DataItem[] = [
   { name: "Emily", value: 34 },
   { name: "Marion", value: 53 },
   { name: "Nicolas", value: 98 },
-]
+];
 
-const colors = ['#98abc5', '#8a89a6', '#7b6888', '#6b486b', '#a05d56'].reverse();
+const colors = [
+  "#98abc5",
+  "#8a89a6",
+  "#7b6888",
+  "#6b486b",
+  "#a05d56",
+].reverse();
 
-export default function PieChartPracticePage() {
-  const sliceRef = useRef<SVGSVGElement>(null)
-  
+export default function DonutChartPage() {
+  const sliceRef = useRef<SVGSVGElement>(null);
+
   // 2. define dimensions
   const dims = {
     height: 500,
@@ -31,46 +37,46 @@ export default function PieChartPracticePage() {
       top: 40,
       right: 40,
       bottom: 40,
-      left: 40
-    }
-  }
+      left: 40,
+    },
+  };
 
-  const boundedWidth = dims.width - dims.margin.right - dims.margin.left
-  const boundedHeight = dims.height - dims.margin.top - dims.margin.bottom
+  const boundedWidth = dims.width - dims.margin.right - dims.margin.left;
+  const boundedHeight = dims.height - dims.margin.top - dims.margin.bottom;
 
-  const radius = Math.min(dims.height, dims.width) / 2 - dims.margin.top
+  const radius = Math.min(dims.height, dims.width) / 2 - dims.margin.top;
 
   // 3. define accessors, scales, and generators
-  const valueAccessor = d => d.value
+  const valueAccessor = (d: DataItem) => d.value;
 
   // 3.a. creates our data
-  const pieGenerator = d3.pie().value(valueAccessor)
-  const pieData = pieGenerator(data)
-    .sort((a, b) => d3.descending(a.value, b.value))
+  const pieGenerator = d3.pie<DataItem>().value(valueAccessor);
+  const pieData = pieGenerator(data).sort((a, b) =>
+    d3.descending(a.value, b.value)
+  );
 
-  const arcGenerator = d3.arc().cornerRadius(8)
+  const arcGenerator = d3.arc().cornerRadius(8);
   const shapes = pieData.map((segment, i) => {
     const shapeInfo = {
       innerRadius: 100,
       outerRadius: radius,
       startAngle: segment.startAngle,
       endAngle: segment.endAngle,
-    }
+    };
 
-    return arcGenerator(shapeInfo)
-  })
+    return arcGenerator(shapeInfo);
+  });
 
   return (
     <svg width={dims.width} height={dims.height}>
-      <g 
-        ref={sliceRef} 
+      <g
+        ref={sliceRef}
         className={style.container}
         style={{
-          transform: 
-            `translate(
+          transform: `translate(
               ${(boundedWidth + dims.margin.left) / 2}px, 
               ${(boundedHeight + dims.margin.top) / 2}px
-            )`
+            )`,
         }}
       >
         {shapes.map((d, i) => {
@@ -89,14 +95,11 @@ export default function PieChartPracticePage() {
                 }
               }}
             >
-              <path
-                d={d as string}
-                fill={colors[i]}
-              />
+              <path d={d as string} fill={colors[i]} />
             </g>
-          )
+          );
         })}
       </g>
     </svg>
-  )
+  );
 }
